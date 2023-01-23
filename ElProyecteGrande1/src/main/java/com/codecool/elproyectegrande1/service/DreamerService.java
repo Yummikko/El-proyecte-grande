d2f94ac44c5173139fa5203ed0c4019ea9211c11
Yummikko <ola.chyla0@gmail.com>
2023-01-23 16:05:20 +0100

    added fundamental java classes

---
+++ ElProyecteGrande1/src/main/java/com/codecool/elproyectegrande1/service/DreamerService.java
@@ -0,0 +1,13 @@
+package com.codecool.elproyectegrande1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElProyecteGrande1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElProyecteGrande1.class, args);
+    }
+
+}
