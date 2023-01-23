aee824f4d0e967408beb9491b365957d4e33c27f
Duc Trung Bui <txbuiduc@gmail.com>
2023-01-23 17:22:46 +0100

    renaming app class

--- ElProyecteGrande1/src/main/java/com/codecool/elproyectegrande1/service/DreamerService.java
+++ ElProyecteGrande1/src/main/java/com/codecool/elproyectegrande1/service/DreamerService.java
@@ -1,13 +1,1 @@
-package com.codecool.elproyectegrande1;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class ElProyecteGrande1 {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ElProyecteGrande1.class, args);
-    }
-
-}
