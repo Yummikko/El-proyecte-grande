52c9596af9590cc25358cea6add7692256866f4c
Duc Trung Bui <txbuiduc@gmail.com>
2023-02-18 16:25:30 +0100

    adding User entity

--- ElProyecteGrande1/dream-catcher-backend/src/main/java/com/codecool/elproyectegrande1/entity/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codecool.elproyectegrande1.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
