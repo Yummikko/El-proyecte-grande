5e6ff1f3ed381509b411debb7004cd155f8d74dc
Duc Trung Bui <txbuiduc@gmail.com>
2023-04-11 03:51:41 +0200

    New Tests

--- ElProyecteGrande1/dream-catcher-backend/src/test/java/com/codecool/elproyectegrande1/repository/CommentRepositoryTest.java
+++ ElProyecteGrande1/dream-catcher-backend/src/test/java/com/codecool/elproyectegrande1/repository/CommentRepositoryTest.java
@@ -7,14 +7,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @RunWith(SpringJUnit4ClassRunner.class)
