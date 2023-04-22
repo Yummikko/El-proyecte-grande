21c676f82d7d4b9eb8432ff87122a7823ae0340f
Duc Trung Bui <txbuiduc@gmail.com>
2023-04-22 14:48:34 +0200

    Merge branch 'merging' into duc-trung-16
    # Conflicts:
    #	ElProyecteGrande1/dream-catcher-backend/src/main/java/com/codecool/elproyectegrande1/service/UserService.java

--- ElProyecteGrande1/dream-catcher-backend/src/main/java/com/codecool/elproyectegrande1/config/springSecurity/WebSecurityConfig.java
+++ ElProyecteGrande1/dream-catcher-backend/src/main/java/com/codecool/elproyectegrande1/config/springSecurity/WebSecurityConfig.java
@@ -18,8 +18,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
-//@Configuration
-//@EnableWebSecurity
 @EnableMethodSecurity(
         prePostEnabled = true)
 public class WebSecurityConfig {
